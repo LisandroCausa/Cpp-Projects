287a8a3acea54b1b5ccf8b933cf9c4c40f3ca00f
Lisi <lisandro.anexa@gmail.com>
2022-03-09 20:44:56 -0300

    Game of Life: Improve practices

--- Conway's Game of Life/main.cpp
+++ Conway's Game of Life/main.cpp
@@ -3,19 +3,19 @@
 #include <sstream>
 
 
-sf::Vector2i searchCellInGrid(sf::Vector2i mouse, unsigned short cellSpace);
+sf::Vector2i searchCellInGrid(const sf::Vector2i mouse, const int cellSpace);
 
 
 int main()
 {
-	const unsigned int screenSizeX = 800;
-	const unsigned int screenSizeY = 800;
+	constexpr int screenSizeX{ 800 };
+	constexpr int screenSizeY{ 800 };
 
 	sf::RenderWindow window(sf::VideoMode(screenSizeX, screenSizeY), "Conway's Game of Life - by Lisandro Causa");
 	window.setFramerateLimit(45);
-	unsigned short updateCount = 1;
+	int updateCount{ 1 };
 
-	const sf::Vector2f pauseRectSize = sf::Vector2f(50, 115);
+	const sf::Vector2f pauseRectSize{ sf::Vector2f(50, 115) };
 
 	sf::RectangleShape pauseRect1(pauseRectSize);
 	pauseRect1.setFillColor(sf::Color::Red);
@@ -38,24 +38,24 @@ int main()
 
 	std::stringstream ss_pop;
 
-	bool pause = false;
-	bool wasKeyPressed = false;
+	bool pause{ false };
+	bool wasKeyPressed{ false };
 
-	const unsigned int size = 72; // grid size
+	constexpr int size{ 72 }; // grid size
 
-	bool grid[size][size];
-	bool temp_grid[size][size];
+	bool grid[size][size]{};
+	bool temp_grid[size][size]{};
 
-	unsigned short neighbours;
+	int neighbours{};
 
-	unsigned int population = 0;
+	int population{ 0 };
 
-	const unsigned short pixelsPerCell = 10;
-	const unsigned short pixelsBetweenCells = 1;
+	constexpr int pixelsPerCell{ 10 };
+	constexpr int pixelsBetweenCells{ 1 };
 
-	for (unsigned int y = 0; y < size; y++) // Start all cells at 0
+	for (int y{ 0 }; y < size; ++y) // Start all cells at false (dead)
 	{
-		for (unsigned int x = 0; x < size; x++)
+		for (int x{ 0 }; x < size; ++x)
 		{
 			grid[y][x] = false;
 			temp_grid[y][x] = false;
@@ -77,7 +77,8 @@ int main()
 				window.close();
 		}
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) 
+			window.close();
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 		{
@@ -97,76 +98,69 @@ int main()
 			sf::Mouse::getPosition(window).y > 0 &&
 			sf::Mouse::getPosition(window).y < screenSizeY)
 		{
+			sf::Vector2i cell{ searchCellInGrid(sf::Mouse::getPosition(window), pixelsPerCell + pixelsBetweenCells) };
 			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 			{
-				sf::Vector2i* cellXY = new sf::Vector2i;
-				*cellXY = searchCellInGrid(sf::Mouse::getPosition(window), pixelsPerCell + pixelsBetweenCells);
-				grid[cellXY->y][cellXY->x] = true;
-				delete cellXY;
+				grid[cell.y][cell.x] = true;
 			}
 			else if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
 			{
-				sf::Vector2i* cellXY = new sf::Vector2i;
-				*cellXY = searchCellInGrid(sf::Mouse::getPosition(window), pixelsPerCell + pixelsBetweenCells);
-				grid[cellXY->y][cellXY->x] = false;
-				delete cellXY;
+				grid[cell.y][cell.x] = false;
 			}
 		}
 
 		window.clear(sf::Color::Black);
 
 		population = 0;
-		for (unsigned int y = 0; y < size; y++)
+		for (int y{ 0 }; y < size; ++y)
 		{
-			for (unsigned int x = 0; x < size; x++)
+			for (int x{ 0 }; x < size; ++x)
 			{
-				if (grid[y][x])
-				{
-					sf::RectangleShape* cell = new sf::RectangleShape(sf::Vector2f(pixelsPerCell, pixelsPerCell));
-					cell->setPosition(x * (pixelsPerCell + pixelsBetweenCells), y * (pixelsPerCell + pixelsBetweenCells));
-					window.draw(*cell);
-					delete cell;
-					population++;
-				}
+				if (!grid[y][x])
+					continue;
+				sf::RectangleShape cell(sf::Vector2f(pixelsPerCell, pixelsPerCell));
+				cell.setPosition(static_cast<float>(x * (pixelsPerCell + pixelsBetweenCells)), static_cast<float>(y * (pixelsPerCell + pixelsBetweenCells)));
+				window.draw(cell);
+				++population;
 			}
 		}
 
 		if (!pause)
 		{
-			updateCount--;
+			--updateCount;
 			if (updateCount == 0)
 			{
 				updateCount = 4;
-				for (unsigned int y = 0; y < size; y++)
+				for (int y{ 0 }; y < size; ++y)
 				{
-					for (unsigned int x = 0; x < size; x++)
+					for (int x{ 0 }; x < size; ++x)
 					{
 						neighbours = 0;
 						if (y > 0)
 						{
 							if (x > 0)
-								if (grid[y - 1][x - 1]) neighbours++;
+								if (grid[y - 1][x - 1]) ++neighbours;
 
 							if (x < size - 1)
-								if (grid[y - 1][x + 1]) neighbours++;
+								if (grid[y - 1][x + 1]) ++neighbours;
 
-							if (grid[y - 1][x]) neighbours++;
+							if (grid[y - 1][x]) ++neighbours;
 						}
 						if (x > 0)
-							if (grid[y][x - 1]) neighbours++;
+							if (grid[y][x - 1]) ++neighbours;
 
 						if (x < size - 1)
-							if (grid[y][x + 1]) neighbours++;
+							if (grid[y][x + 1]) ++neighbours;
 
 						if (y < size - 1)
 						{
 							if (x > 0)
-								if (grid[y + 1][x - 1]) neighbours++;
+								if (grid[y + 1][x - 1]) ++neighbours;
 
 							if (x < size - 1)
-								if (grid[y + 1][x + 1]) neighbours++;
+								if (grid[y + 1][x + 1]) ++neighbours;
 
-							if (grid[y + 1][x]) neighbours++;
+							if (grid[y + 1][x]) ++neighbours;
 						}
 
 						if (grid[y][x])
@@ -176,9 +170,9 @@ int main()
 					}
 				}
 
-				for (unsigned int y = 0; y < size; y++)
+				for (int y{ 0 }; y < size; ++y)
 				{
-					for (unsigned int x = 0; x < size; x++)
+					for (int x{ 0 }; x < size; ++x)
 					{
 						grid[y][x] = temp_grid[y][x];
 						temp_grid[y][x] = false;
@@ -203,10 +197,8 @@ int main()
 }
 
 
-sf::Vector2i searchCellInGrid(sf::Vector2i mouse, unsigned short cellSpace)
+sf::Vector2i searchCellInGrid(const sf::Vector2i mouse, const int cellSpace)
 {
-	sf::Vector2i result;
-	result.x = int(mouse.x / cellSpace);
-	result.y = int(mouse.y / cellSpace);
+	sf::Vector2i result( static_cast<int>(static_cast<double>(mouse.x) / cellSpace), static_cast<int>(static_cast<double>(mouse.y) / cellSpace));
 	return result;
 }
